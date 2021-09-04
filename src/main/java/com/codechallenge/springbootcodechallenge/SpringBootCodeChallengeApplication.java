b2b72300929c67c92a3184c3ac0cf72458f82e43
parvizmovahedan <pmovahedan@gmail.com>
2021-09-04 15:32:09 -0400

    initial spring boot commit

---
+++ src/main/java/com/codechallenge/springbootcodechallenge/SpringBootCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.springbootcodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codechallenge/springbootcodechallenge/SpringBootCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.springbootcodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
